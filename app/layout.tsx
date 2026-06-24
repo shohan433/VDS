c6baa0ae2f26e45277488e580b599368ebb90060
shohan433 <shohanchowdhury433510@gmail.com>
2026-06-24 16:49:29 +1000

    Add Google Analytics, update contact info, install @next/third-parties

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
     >
+      <GoogleAnalytics gaId="G-NTX51SXW20" />
       <body className="min-h-full flex flex-col bg-[#050b16] text-[#f8fafc] font-sans m-0">
         {children}
       </body>
